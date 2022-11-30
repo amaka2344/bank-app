226b3a4e8d10a136f42115ae58f917c60b069bb4
Solutech11 <soluwizy@gmail.com>
2022-11-30 20:27:45 +0100

    doing Reg

--- screens/Auth/register.js
+++ screens/Auth/register.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { SafeAreaView } from 'react-native-safe-area-context'
 const Register = () => {
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <SafeAreaView style={{flex:1, backgroundColor:'white'}}>
+      <Pressable>
+        {/* higher */}
+        <View>
+          <Text style={{
+            color:'purple',
+            fontSize:25,
+            fontWeight:'400',
+            padding:20,
+            fontFamily:'Montserrat_700Bold'
+
+          }}>Transferly</Text>
+        </View>
+        <View>
+          <Text style={{
+            color:'#4c484a',
+            fontSize:26,
+            fontWeight:'400',
+            fontFamily:'Montserrat_700Bold',
+            paddingHorizontal: 20,
+            marginBottom:20
+          }}>Create an Account</Text>
+        </View>
+        <View>
+          
+        </View>
+      </Pressable>
+    </SafeAreaView>
   )
 }
 
